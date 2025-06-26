004268db07c1542702dc7319c5d26571ee0a27d2
Ravindu Ranathilaka <ravindu925@gmail.com>
2025-06-26 20:07:40 +0900

    https secure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\URL;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -20,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if(\APP::environment(['production'])){
+        if(\App::environment(['production'])){
             \URL::forceScheme('https');
         }
     }
