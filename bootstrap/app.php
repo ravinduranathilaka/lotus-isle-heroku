f06656ee49f764bb1f16455c1dbd87f1eebe81fe
Ravindu Ranathilaka <ravindu925@gmail.com>
2025-06-28 10:57:45 +0900

    undo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\CustomerMiddleware;
+use App\Http\Middleware\ForceHttps;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,11 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
         $middleware->alias([
             'admin' => AdminMiddleware::class,
             'customer' => CustomerMiddleware::class,
         ]);
+        $middleware->appendToGroup('web', ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
